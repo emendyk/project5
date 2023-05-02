6e3cc66d132b76f174f846f862e814def73942a6
emendyk <emendyk@guilford.edu>
2023-05-02 17:18:15 -0400

    project5

---
+++ src/main/java/edu/guilford/Clothes.java
@@ -0,0 +1,116 @@
+package edu.guilford;
+
+public abstract class Clothes implements Comparable<Clothes> {
+    private String color;
+    private int size;
+    private String type;
+    
+    public Clothes(String color, int size, String type) {
+        this.color = color;
+        this.size = size;
+        this.type = type;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+    
+    public int getSize() {
+        return size;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    // An inherited method that can be overridden by subclasses
+    public void wash() {
+        System.out.println("Washing " + getType() + " - " + getColor() + " - " + getSize());
+    }
+    
+    // An abstract method that will be implemented by each subclass
+    public abstract void wear();
+    
+    // A method inherited by each subclass
+    public int compareTo(Clothes other) {
+        if (this.getSize() > other.getSize()) {
+            return 1;
+        } else if (this.getSize() < other.getSize()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+interface Wearable {
+    public String getMaterial();
+}
+
+class Shirt extends Clothes implements Wearable {
+    private String material;
+    
+    public Shirt(String color, int size, String material) {
+        super(color, size, "Shirt");
+        this.material = material;
+    }
+    
+    public String getMaterial() {
+        return material;
+    }
+    
+    public void wear() {
+        System.out.println("Wearing shirt made of " + getMaterial());
+    }
+}
+
+class Jacket extends Clothes implements Wearable {
+    private String material;
+    
+    public Jacket(String color, int size, String material) {
+        super(color, size, "Jacket");
+        this.material = material;
+    }
+    
+    public String getMaterial() {
+        return material;
+    }
+    
+    public void wear() {
+        System.out.println("Wearing jacket made of " + getMaterial());
+    }
+}
+
+class Shorts extends Clothes implements Wearable {
+    private String material;
+    
+    public Shorts(String color, int size, String material) {
+        super(color, size, "Shorts");
+        this.material = material;
+    }
+    
+    public String getMaterial() {
+        return material;
+    }
+    
+    public void wear() {
+        System.out.println("Wearing shorts made of " + getMaterial());
+    }
+}
+
+class Pants extends Clothes implements Wearable {
+    private String material;
+    
+    public Pants(String color, int size, String material) {
+        super(color, size, "Pants");
+        this.material = material;
+    }
+    
+    public String getMaterial() {
+        return material;
+    }
+    
+    public void wear() {
+        System.out.println("Wearing pants made of " + getMaterial());
+    }
+}
---
+++ src/main/java/edu/guilford/Main.java
@@ -0,0 +1,72 @@
+package edu.guilford;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        ArrayList<Clothes> clothes = new ArrayList<Clothes>();
+        
+        // Instantiate at least three objects of each subclass with different values for the attributes
+        clothes.add(new Shirt("Red", 6, "Cotton"));
+        clothes.add(new Shirt("Blue", 8, "Polyester"));
+        clothes.add(new Shirt("Green", 8, "Silk"));
+        
+        clothes.add(new Jacket("Black", 14, "Leather"));
+        clothes.add(new Jacket("Brown", 16, "Denim"));
+        clothes.add(new Jacket("Gray", 18, "Wool"));
+        
+        clothes.add(new Shorts("Yellow", 6, "Denim"));
+        clothes.add(new Shorts("Orange", 8, "Cotton"));
+        clothes.add(new Shorts("Purple", 10, "Linen"));
+        
+        clothes.add(new Pants("White", 12, "Jeans"));
+        clothes.add(new Pants("Pink", 14, "Leggings"));
+        clothes.add(new Pants("Beige", 16, "Cargo"));
+        
+        // Create labels to display the results
+        Label washLabel = new Label("Washing:");
+        Label wearLabel = new Label("Wearing:");
+        Label sortedLabel = new Label("Sorted by size:");
+        Label resultLabel = new Label();
+        
+        // Demonstrate the use of one inherited method by objects of each subclass
+        for (Clothes c : clothes) {
+            c.wash();
+            washLabel.setText(washLabel.getText() + "\n" + c.getType() + " - " + c.getColor() + " - " + c.getSize());
+        }
+        
+        // Demonstrate the use of one abstract method by objects of each subclass
+        for (Clothes c : clothes) {
+            c.wear();
+            wearLabel.setText(wearLabel.getText() + "\n" + c.getType() + " - " + c.getColor() + " - " + c.getSize() + " - " + ((Wearable)c).getMaterial());
+        }
+        
+        // Show that the Comparable interface has been implemented correctly by using compareTo to order the objects of each subclass
+        Collections.sort(clothes);
+        for (Clothes c : clothes) {
+            sortedLabel.setText(sortedLabel.getText() + "\n" + c.getType() + " - " + c.getSize());
+        }
+        
+        // Create a VBox to hold the labels
+        VBox vbox = new VBox(washLabel, wearLabel, sortedLabel, resultLabel);
+        
+        // Create a Scene and add the VBox to it
+        Scene scene = new Scene(vbox, 400, 400);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
